01269a8453c0c1b9108b33d6371c2a74462a8493
Тарахомин Юрій <tarachom@gmail.com>
2026-05-18 16:12:07 +0300

    Пошук dll при запуску

--- Program.cs
+++ Program.cs
@@ -3,12 +3,21 @@ using Gtk;
 using Gdk;
 using GeneratedCode.Довідники;
 
+using System.Runtime.InteropServices;
+
 namespace StorageAndTrade;
+
+internal static partial class NativeMethods
+{
+    [LibraryImport("kernel32.dll", EntryPoint = "SetDllDirectoryW", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool SetDllDirectory(string lpPathName);
+}
 
 class Program
 {
     public static readonly Application BasicApp = Application.New("ua.org.accounting.storage_and_trade", Gio.ApplicationFlags.FlagsNone);
-
+    
     /// <summary>
     /// Основна форма
     /// </summary>
@@ -21,6 +30,19 @@ class Program
 
     static void Main()
     {
+        if (OperatingSystem.IsWindows())
+        {
+            string msysBinPath = @"C:\msys64\ucrt64\bin";
+
+            if (System.IO.Directory.Exists(msysBinPath))
+            {
+                if (!NativeMethods.SetDllDirectory(msysBinPath))
+                    Console.WriteLine("Warning: Failed to set DLL directory.");
+            }
+            else
+                Console.WriteLine($"Warning: MSYS2 path not found at {msysBinPath}");
+        }
+
         BasicApp.OnActivate += (app, _) =>
         {
             var window = FormConfigurationSelection.New();
@@ -34,7 +56,7 @@ class Program
             //Icon
             IconTheme iconTheme = IconTheme.GetForDisplay(display);
             iconTheme.AddSearchPath(Path.Combine(AppContext.BaseDirectory, "images"));
-            
+
             //Css
             string styleDefaultFile = Path.Combine(AppContext.BaseDirectory, "StyleCss/Default.css");
             if (File.Exists(styleDefaultFile))
